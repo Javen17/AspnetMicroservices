b57ee608aaafd5206b7cc72db42bfc81d765f23b
Javier Oviedo <javieroviedo2000@gmail.com>
2022-02-13 13:25:21 -0600

    feat: discount api microservice

---
+++ source/Services/Discount/Discount.api/Controllers/DiscountController.cs
@@ -0,0 +1,52 @@
+﻿using Discount.api.Entities;
+using Discount.api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.api.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class DiscountController : ControllerBase
+	{
+		private readonly IDiscountRepository _repository;
+
+		public DiscountController(IDiscountRepository repository)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+		}
+
+		[HttpGet("{productName}", Name = "GetDiscount")]
+		[ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<bool>> GetDiscount(string productName)
+		{
+			var discount = await _repository.GetDiscount(productName);
+			return Ok(discount);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(ActionResult<Coupon>), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> CreateDiscount(Coupon coupon)
+		{
+			await _repository.CreateDiscount(coupon);
+			return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+		}
+
+		[HttpPut]
+		[ProducesResponseType(typeof(ActionResult<Coupon>), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> UpdateDiscount(Coupon coupon)
+		{
+			return Ok(await _repository.UpdateDiscount(coupon));
+		}
+
+		[HttpDelete("{productName}", Name = "GetDiscount")]
+		[ProducesResponseType(typeof(ActionResult<Coupon>), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+		{
+			return Ok(await _repository.DeleteDiscount(productName));
+		}
+
+	}
+}
---
+++ source/Services/Discount/Discount.api/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Discount.api.Extensions;
+
+namespace Discount.api
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
+			host.MigrateDatabase<Program>();
+			host.Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
